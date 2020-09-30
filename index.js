a6c7a5d32a620d742cdcdcb5d1b0d79df0c23d77
fjqm941025 <fjqm941025@naver.com>
2020-09-30 18:52:40 +0900

    install cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 const PORT = 3000;
 
@@ -11,23 +13,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./public/index.html"));
 });
 
-app.options("/", (req, res) => {
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "POST, GET, OPTIONS, DELETE, PUT"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Origin", "http://3.134.106.234:3000/");
-
-  res.send(200);
-});
-
 app.post("/", (req, res) => {
   const body = req.body;
 
   console.log(body);
-
-  res.setHeader("Access-Control-Allow-Origin", "http://3.134.106.234:3000/");
 
   res.send("2post!");
 });
