259057af6dbd0de3fd8fd432d6b5bcf03db47008
fjqm941025 <fjqm941025@naver.com>
2020-09-30 18:47:54 +0900

    resolve cors

--- index.js
+++ index.js
@@ -11,10 +11,23 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./public/index.html"));
 });
 
+app.options("/", (req, res) => {
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, GET, OPTIONS, DELETE, PUT"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+
+  res.send(200);
+});
+
 app.post("/", (req, res) => {
   const body = req.body;
 
   console.log(body);
+
+  response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
 
   res.send("2post!");
 });
